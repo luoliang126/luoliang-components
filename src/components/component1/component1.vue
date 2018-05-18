3e9073fc4841d4d6e309aae78485fa9fb368c435
luoliang <394500842@qq.com>
2018-05-18 18:36:51 +0800

    增加项目

---
+++ src/components/component1/component1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:'luoliang1'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/components/component2/component2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:'luoliang2'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
